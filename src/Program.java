e436b94c9e0efa5091c3e9356cf9835e8c6d6d61
Ivan <ngk-trade@mail.ru>
2017-10-14 22:00:27 +0300

    розпарсили в объект

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,5 @@
+
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import youtubeProject.channels.ChannelListResponse;
@@ -5,6 +7,7 @@ import youtubeProject.channels.OneChannel;
 import youtubeProject.channels.Request1;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -13,15 +16,16 @@ import java.util.Scanner;
  * Created by 1 on 03.10.2017.
  */
 public class Program {
-
-
+    Scanner in = new Scanner(System.in);
+static ChannelListResponse channelListResponse;
+    static String channelId;
 
 
 
     public static void main(String[] args) throws UnirestException, IOException {
         Scanner in = new Scanner(System.in);
         System.out.print("Input Channel ID   ");
-        String channelId = in.nextLine();
+        channelId = in.nextLine();
         //readFileToString(channelId);
 
         String jsonString = Request1.getChannelDataAsString(channelId);
@@ -34,7 +38,13 @@ public class Program {
 
         System.out.println("Json string was converted to object.\nFinish.\n");
         readFileToString(channelId);
-        convertStringToChannel(jsonString);
+        ChannelListResponse channelListResponse1 = convertStringToChannel(jsonString);
+        ArrayList<OneChannel>list1 = (ArrayList<OneChannel>) channelListResponse1.items;
+        for(int i = 0; i < channelListResponse1.items.size(); i++){
+            System.out.println(channelListResponse1.items.get(i).snippet.publishedAt);
+        }
+
+
         //return new Channel(convertStringToChannel(jsonString));
 
 
@@ -45,7 +55,7 @@ public class Program {
 
 
     private static void writeCacheToFile(String jsonString, String channelId) throws IOException {
-        String savePath = "Cache" + channelId + ".txt";
+        String savePath = "Cache/" + channelId + ".txt";
         File file = new File(savePath);
         FileWriter writer = new FileWriter(file);
         writer.write(jsonString);
@@ -54,7 +64,7 @@ public class Program {
     }
 
     static String readFileToString(String channelName) {
-        String cachedChannelPath = ("Cache" + channelName + ".txt");
+        String cachedChannelPath = ("Cache/" + channelName + ".txt");
         StringBuilder result = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(cachedChannelPath))) {
             String line;
@@ -68,7 +78,7 @@ public class Program {
         return result.toString();
     }
 
-    static OneChannel convertStringToChannel(String jsonString) {
+    static ChannelListResponse convertStringToChannel(String jsonString) {
         ObjectMapper objectMapper = new ObjectMapper();
         ChannelListResponse channelsResponse = null;
         try {
@@ -76,16 +86,8 @@ public class Program {
         } catch (IOException e) {
             //alert("IOException thrown","Cannot convert a Json string to an object.");
         }
-        return channelsResponse.items.get(0);}
-
-
-
-
-
-
-
-
-
-
+        return channelsResponse;
     }
+
+}
 
